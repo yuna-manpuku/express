13f7a9ac061d805ae2f4fae779c5a8f6ca0d5f1c
Yuna Tsudo <2401320004bc@cyber-u.ac.jp>
2025-05-18 21:31:56 +0900

    Add Page

--- routes/hello.js
+++ routes/hello.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('helli', { title: 'hello' });
+  res.render('hello', { title: 'hello' });
 });
 
 module.exports = router;
