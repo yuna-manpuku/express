c9c620693db457908bebf649d70edfb8ae1d99b1
Yuna Tsudo <2401320004bc@cyber-u.ac.jp>
2025-06-11 10:56:11 +0900

    add api

--- notes.js
+++ notes.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+
 const cors = require('cors'); // corsミドルウェアを追加
 require('dotenv').config();
 
@@ -22,6 +23,35 @@ const query = { id: 2 };
 const note = await notes.findOne(query);
 
 res.json(note);
+=======
+
+//レスポンスのデータ(ノート全体)
+const responseObjectDataAll = {
+  textObject1:{
+    id:1,
+    title:'ノート1のタイトルです',
+    subTitle:'ノート1のサブタイトルです',
+    bodyText:'ノート1の本文です'
+  },
+  textObject2:{
+    id:2,
+    title:'ノート2のタイトルです',
+    subTitle:'ノート2のサブタイトルです',
+    bodyText:'ノート2の本文です'
+  },
+};
+
+/**
+ * メモを全権取得するAPI
+ * @returns{Object[]} data
+ * @returns{number} data.id -ID
+ * @returns{string} data.title -タイトル
+ * @returns{string} data.text -内容内容
+ */
+
+router.get('/', function(req, res, next) {
+  //全件取得して返す
+  res.json('respondObjectDataAll');
 })
 
 module.exports = router;
