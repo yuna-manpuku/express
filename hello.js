e6759d588ec7a1beea3497cc134a05ab3460f16e
Yuna Tsudo <2401320004bc@cyber-u.ac.jp>
2025-06-11 11:14:51 +0900

    初回コミット

--- hello.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('hello', { title: 'hello' });
-});
-
-module.exports = router;
